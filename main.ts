e4628ac81de73c8ca88a834cafdf1b43a53015bf
Jeff McMahan <jeffrey.lee.mcmahan@gmail.com>
2024-10-08 14:48:56 -0400

    Update main.ts
    ...

--- main.ts
+++ main.ts
@@ -5,7 +5,11 @@ window.addEventListener('scroll', () => {
     function unscroll() {
         setTimeout(() => window.scrollTo(0, 0), 100)
         focusedElement.removeEventListener('blur', unscroll)
+        document.activeElement.removeAttribute('willUnscroll')
     }
-    
-    focusedElement.addEventListener('blur', unscroll)
+
+    if (!document.activeElement.hasAttribute('willUnscroll')) {
+        document.activeElement.setAttribute('willUnscroll', '1')
+        focusedElement.addEventListener('blur', unscroll)
+    }
 })
